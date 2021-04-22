dd4c4f33ccf3a4d016c2627c5ef25ced2f1a716c
gumsu <chavi9@naver.com>
2021-04-22 13:12:23 +0900

    [#7] db로부터 이미지 정보를 읽어오기 위한 함수 추가

--- src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductImageRepository.kt
+++ src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductImageRepository.kt
@@ -2,5 +2,10 @@ package com.gdh.shoppingmall.domain.product
 
 import org.springframework.data.jpa.repository.JpaRepository
 
+/**
+ * findByIdIn() -> 데이터베이스로부터 이미지 정보를 읽어오기 위해 추가
+ */
+
 interface ProductImageRepository: JpaRepository<ProductImage, Long> {
+    fun findByIdIn(imageIds: List<Long>): MutableList<ProductImage>
 }
