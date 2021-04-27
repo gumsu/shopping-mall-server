0da4adfc9569e73a88481f8d2b134e94e9efb7a6
gumsu <chavi9@naver.com>
2021-04-27 18:00:13 +0900

    [#8] 쿼리 기반으로 상품 리스트를 읽어오는 로직 작성

--- src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductRepository.kt
+++ src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductRepository.kt
@@ -1,7 +1,7 @@
 package com.gdh.shoppingmall.domain.product
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import java.awt.print.Pageable
 
 /**
  * 상품 정보 읽어오는 레포지토리 - 상품을 카테고리별로, 스크롤에 다라 id 값을 기준으로 전/후를 읽어야 하므로
