13f205917d51f05d05e72c257bcc7dfddc251d9a
gumsu <chavi9@naver.com>
2021-10-03 20:33:58 +0900

    [#13] 키워드 검색 함수 선언

--- src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductRepository.kt
+++ src/main/kotlin/com/gdh/shoppingmall/domain/product/ProductRepository.kt
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.JpaRepository
  * findByCategoryIdAndIdLessThanOrderByIdDesc
  * 상품 리스트가 아래쪽으로 스크롤될 때 호출되는 함수, 예전 데이터부터 읽음, 내림차순
  *
- * Pageable -> 검색 조건을 기준으로 페이지당 몇 개의 값을 읽을 것인지 설정정
+ * Pageable -> 검색 조건을 기준으로 페이지당 몇 개의 값을 읽을 것인지 설정
 */
 
 interface ProductRepository: JpaRepository<Product, Long> {
@@ -22,4 +22,10 @@ interface ProductRepository: JpaRepository<Product, Long> {
 
     fun findByCategoryIdAndIdLessThanOrderByIdDesc(
         categoryId: Int?, id: Long, pageable: Pageable): List<Product>
+
+    fun findByIdGreaterThanAndNameLikeOrderByIdDesc(
+        id: Long, keyword: String, pageable: Pageable): List<Product>
+
+    fun findByIdLessThanAndNameLikeOrderByIdDesc(
+        id: Long, keyword: String, pageable: Pageable): List<Product>
 }
